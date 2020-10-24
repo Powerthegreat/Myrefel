506029d60c2132efeff7be3321c0cc187c173dd4
Power <powerfinnthegreat@gmail.com>
2020-10-25 08:27:11 +1100

    Updated register to check if user is registered and add data to database

--- cogs/account.py
+++ cogs/account.py
@@ -12,8 +12,12 @@ class Account(commands.Cog):
 		aliases=['start']
 	)
 	async def register(self, ctx):
-		print(f'{ctx.message.author} registered')
-		await ctx.send('Welcome to Myrefel!')
+		if (len(self.bot.database.execute(f'SELECT Id FROM chars WHERE Id=\'{ctx.message.author.id}\'').fetchall()) <= 0):
+			self.bot.database.execute(f'INSERT INTO chars (Id, Name) VALUES ({ctx.message.author.id}, \'{ctx.message.author.name}\')')
+			print(f'{ctx.message.author} registered')
+			await ctx.send('Welcome to Myrefel!')
+		else:
+			await ctx.send('You have already registered!')
 
 def setup(bot):
 	bot.add_cog(Account(bot))
