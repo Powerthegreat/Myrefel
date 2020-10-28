e11bb0dfb772b0770d9429fec6adb06e3a0f1e21
Power <powerfinnthegreat@gmail.com>
2020-10-28 16:12:04 +1100

    Updated move command

--- cogs/world_interaction.py
+++ cogs/world_interaction.py
@@ -24,11 +24,31 @@ class WorldInteraction(commands.Cog):
 	@commands.command(
 		name='move',
 		description='Moves you to a different location\nIf no location is specified, shows a list of accessible locations',
-		usage='[destination]'
+		usage='[destination id]'
 	)
-	async def move(self, ctx):
-		if myrefeldb.GetPlayerData(self, ctx.message.author.id) != None:
-			await ctx.send('WIP')
+	async def move(self, ctx, *args):
+		playerData = myrefeldb.GetPlayerData(self, ctx.message.author.id)
+		if playerData != None:
+			connections = self.bot.database.execute(f'SELECT FirstRoom, SecondRoom from roomconns WHERE FirstRoom = {playerData[2]};').fetchall()
+			message = ''
+			if not args:
+				if len(connections) > 0:
+					message = 'Available locations:\n'
+					for i in range(0, len(connections)):
+						message += str(i) + ': ' + self.bot.database.execute(f'SELECT name FROM rooms WHERE Id = {connections[i][1]};').fetchall()[0][0] + '\n'
+				else:
+					message = 'No available locations'
+			else:
+				if int(args[0]) < len(connections):
+					destination = connections[int(args[0])][1]
+					destinationName = self.bot.database.execute(f'SELECT name FROM rooms WHERE Id = {destination};').fetchall()[0][0]
+					message = 'Moved to ' + destinationName
+					self.bot.database.execute(f'UPDATE chars SET Room = \'{destination}\' WHERE Id = {ctx.message.author.id};')
+					self.bot.database.commit()
+					myrefeldebug.DebugLog(f'{ctx.message.author} moved to {destinationName}')
+				else:
+					message = 'Invalid destination!'
+			await ctx.send(message)
 		else:
 			await ctx.send('You are not registered!')
 	
