87c6ec1b4db3aedd3d2d46b69882e9f607753693
Power <powerfinnthegreat@gmail.com>
2020-10-28 16:51:59 +1100

    Added teleport other command

--- cogs/dev.py
+++ cogs/dev.py
@@ -57,6 +57,39 @@ class Dev(commands.Cog):
 	async def reload_db_error(self, ctx, error):
 		if isinstance(error, commands.NotOwner):
 			myrefeldebug.DebugLog(f'{ctx.message.author} tried to load database changes')
+	
+	@commands.command(
+		name='moveother',
+		description='Moves another user.',
+		aliases=['tpother', 'tpo'],
+		hidden=True
+	)
+	@commands.is_owner()
+	async def moveother(self, ctx, *args):
+		if myrefeldb.GetPlayerData(self, ctx.message.author.id) != None:
+			if not args:
+				await ctx.send('You must specify a room id.')
+				return
+
+			roomId = int(args[0])
+			roomName = self.bot.database.execute(f'SELECT name FROM rooms WHERE Id = {roomId};').fetchall()
+			target = ctx.message.mentions[0]
+			if len(roomName) > 0:
+				roomName = roomName[0][0]
+				self.bot.database.execute(f'UPDATE chars SET Room = \'{roomId}\' WHERE Id = {target.id};')
+				self.bot.database.commit()
+				await ctx.send(f'Teleported {target.mention} to {roomName}')
+				myrefeldebug.DebugLog(f'{ctx.message.author} teleported {target} to room {roomName}')
+			else:
+				await ctx.send(f'Invalid room id!')
+		else:
+			await ctx.send('You are not registered!')
+	
+	@moveother.error
+	async def moveother_error(self, ctx, error):
+		myrefeldebug.DebugLog(str(error))
+		if isinstance(error, commands.NotOwner):
+			myrefeldebug.DebugLog(f'{ctx.message.author} tried to move another user')
 
 def setup(bot):
 	bot.add_cog(Dev(bot))
