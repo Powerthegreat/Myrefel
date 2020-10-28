84706dcbbcaee50859539e156e14a15f16c20a8d
Power <powerfinnthegreat@gmail.com>
2020-10-28 12:13:56 +1100

    Database 1001 - Added room descriptions and tavern room

--- myrefeldb.py
+++ myrefeldb.py
@@ -1,17 +1,31 @@
 def InitDB(database):
-	database.execute('CREATE TABLE IF NOT EXISTS rooms (Id INT NOT NULL, \
-		PRIMARY KEY (Id));')
-	database.execute('CREATE TABLE IF NOT EXISTS chars (Id INT NOT NULL, \
-		Name VARCHAR(255) NOT NULL DEFAULT \'\', \
-		Room INT NOT NULL DEFAULT 0, \
-			PRIMARY KEY (Id), \
-			CONSTRAINT Room FOREIGN KEY (Room) REFERENCES rooms (Id));')
-	database.execute('CREATE TABLE IF NOT EXISTS `world` (Id INT NOT NULL, \
-		Version INT NOT NULL DEFAULT 1000, \
-			PRIMARY KEY (Id));')
-	if len(database.execute('SELECT * FROM world WHERE Id = 0').fetchall()) <= 0:
-		database.execute('INSERT INTO world (Id, Version) VALUES (0, 1000);')
+	worldVersion = database.execute('SELECT Version FROM world WHERE Id = 0').fetchall()
+	if len(worldVersion) <= 0:
+		# Initialise the database
+		database.execute('CREATE TABLE IF NOT EXISTS rooms (Id INT NOT NULL, \
+			Description VARCHAR(2048) NOT NULL DEFAULT \'\', \
+				PRIMARY KEY (Id));')
+		database.execute('CREATE TABLE IF NOT EXISTS chars (Id INT NOT NULL, \
+			Name VARCHAR(255) NOT NULL DEFAULT \'\', \
+			Room INT NOT NULL DEFAULT 0, \
+				PRIMARY KEY (Id), \
+				CONSTRAINT Room FOREIGN KEY (Room) REFERENCES rooms (Id));')
+		database.execute('CREATE TABLE IF NOT EXISTS `world` (Id INT NOT NULL, \
+			Version INT NOT NULL DEFAULT 0, \
+				PRIMARY KEY (Id));')
+		database.execute('INSERT INTO world (Id, Version) VALUES (0, 1001);')
+		AddRooms(database)
+	else:
+		# Update from 1000 to 1001 - adding room descriptions, added tavern room
+		if (worldVersion[0][0] == 1000):
+			database.execute('ALTER TABLE rooms \
+				ADD COLUMN Description VARCHAR(2048) NOT NULL DEFAULT \'\';')
+			database.execute('UPDATE world SET Version = 1001 WHERE  Id = 0;')
+			database.execute('INSERT INTO rooms (Id, Description) VALUES (0, \'A tavern with no distinguishing features.\');')
 	database.commit()
+
+def AddRooms(database):
+	database.execute('INSERT INTO rooms (Id, Description) VALUES (0, \'A tavern with no distinguishing features.\');')
 
 def GetPlayerData(self, playerId):
 	playerData = self.bot.database.execute(f'SELECT * FROM chars WHERE Id = {playerId};').fetchall()
