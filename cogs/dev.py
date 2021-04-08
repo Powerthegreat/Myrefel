1c0ede57567ba1f4527224e02426d5e46b612d0b
Power <lerossignol.erelyn@gmail.com>
2021-04-09 07:19:40 +1000

    Added giveitem

--- cogs/dev.py
+++ cogs/dev.py
@@ -67,7 +67,7 @@ class Dev(commands.Cog):
 		name='moveother',
 		description='Moves another user.',
 		aliases=['tpother', 'tpo'],
-		usage='<room id> <@target>',
+		usage='<room id> <@user>',
 		hidden=True
 	)
 	@commands.is_owner()
@@ -101,6 +101,56 @@ class Dev(commands.Cog):
 		myrefeldebug.DebugLog(str(error))
 		if isinstance(error, commands.NotOwner):
 			myrefeldebug.DebugLog(f'{ctx.author} tried to move another user')
+	
+	@commands.command(
+		name='giveitem',
+		description='Gives a user an item.',
+		aliases=['give'],
+		usage='<item id> <count> <@user>',
+		hidden=True
+	)
+	@commands.is_owner()
+	async def giveitem(self, ctx, *args):
+		# Gives an item to a player
+		if len(ctx.message.mentions) != 1:
+			await ctx.send('You must mention exactly one user to give the item to.')
+			return
+		if myrefeldb.GetPlayerData(self, ctx.message.mentions[0].id) != None:
+			if not args:
+				await ctx.send('You must specify an item id.')
+				return
+
+			itemId = int(args[0])
+			countToAdd = 1
+			if len(args) > 2:
+				countToAdd = int(args[1])
+			itemName = self.bot.database.execute(f'SELECT name FROM items WHERE Id = {itemId};').fetchall()
+			target = ctx.message.mentions[0]
+			if len(itemName) > 0:
+				itemName = itemName[0][0]
+				# Give the item itself
+				itemCount = self.bot.database.execute(f'SELECT count FROM inventory WHERE CharId = {target.id} AND ItemId = {itemId};').fetchall()
+				if len(itemCount) > 0:
+					self.bot.database.execute(f'UPDATE inventory SET count = \'{itemCount[0][0] + countToAdd}\' WHERE CharId = {target.id} AND ItemId = {itemId};')
+				else:
+					self.bot.database.execute(f'INSERT INTO inventory (CharId, ItemId, Count) \
+						VALUES ({target.id}, {itemId}, {countToAdd});')
+				self.bot.database.commit()
+				await ctx.send(f'Gave {target.mention} {countToAdd} of {itemName}')
+				myrefeldebug.DebugLog(f'{ctx.author} gave {target.mentions[0]} {countToAdd} of {itemName}')
+				#self.bot.database.execute(f'UPDATE chars SET Room = \'{roomId}\' WHERE Id = {target.id};')
+				#await ctx.send(f'Teleported {target.mention} to {itemName}')
+				#myrefeldebug.DebugLog(f'{ctx.author} teleported {target} to room {roomName}')
+			else:
+				await ctx.send(f'Invalid item id!')
+		else:
+			await ctx.send('The target is not registered!')
+	
+	@giveitem.error
+	async def giveitem_error(self, ctx, error):
+		myrefeldebug.DebugLog(str(error))
+		if isinstance(error, commands.NotOwner):
+			myrefeldebug.DebugLog(f'{ctx.author} tried to give an item to a player')
 
 def setup(bot):
 	bot.add_cog(Dev(bot))
